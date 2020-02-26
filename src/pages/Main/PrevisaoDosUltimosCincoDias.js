9fd034eedf8670f92c9c0c275607dd19b349ec8a
Angelo Medeiros <angelomedeiros3@gmail.com>
2020-02-26 18:46:18 -0300

    Finalizando o layout da parte central

--- src/pages/Main/PrevisaoDosUltimosCincoDias.js
+++ src/pages/Main/PrevisaoDosUltimosCincoDias.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import search from '../../assets/search.png';
+import rain from '../../assets/few_clouds.png';
 
 export default () => (
     <div className="previsao-dos-ultimos-dias">
@@ -16,7 +17,84 @@ export default () => (
             <img src={search} alt="search" />
         </div>
         <div className="previsao-dos-ultimos-dias__section">
-            <h3>Previsão para a semana:</h3>
+            <h3>Previsão para os próximos 5 dias:</h3>
+            <div className="container">
+                <div className="card">
+                    <div className="card__header">
+                        <img src={rain} alt="clima" />
+                        <p>SEG</p>
+                    </div>
+                    <div className="card__temperatura">
+                        <p>
+                            24.7<span>ºC</span>
+                        </p>
+                    </div>
+                    <div className="card__stats">
+                        <p>3.15 m/s</p>
+                        <p>clouds: 33%</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card__header">
+                        <img src={rain} alt="clima" />
+                        <p>SEG</p>
+                    </div>
+                    <div className="card__temperatura">
+                        <p>
+                            24.7<span>ºC</span>
+                        </p>
+                    </div>
+                    <div className="card__stats">
+                        <p>3.15 m/s</p>
+                        <p>clouds: 33%</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card__header">
+                        <img src={rain} alt="clima" />
+                        <p>SEG</p>
+                    </div>
+                    <div className="card__temperatura">
+                        <p>
+                            24.7<span>ºC</span>
+                        </p>
+                    </div>
+                    <div className="card__stats">
+                        <p>3.15 m/s</p>
+                        <p>clouds: 33%</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card__header">
+                        <img src={rain} alt="clima" />
+                        <p>SEG</p>
+                    </div>
+                    <div className="card__temperatura">
+                        <p>
+                            24.7<span>ºC</span>
+                        </p>
+                    </div>
+                    <div className="card__stats">
+                        <p>3.15 m/s</p>
+                        <p>clouds: 33%</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card__header">
+                        <img src={rain} alt="clima" />
+                        <p>SEG</p>
+                    </div>
+                    <div className="card__temperatura">
+                        <p>
+                            24.7<span>ºC</span>
+                        </p>
+                    </div>
+                    <div className="card__stats">
+                        <p>3.15 m/s</p>
+                        <p>clouds: 33%</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 );
