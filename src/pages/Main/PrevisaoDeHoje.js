dbb34de1888ed8bef7374398ba8344eef85d5aee
Angelo Medeiros <angelomedeiros3@gmail.com>
2020-02-26 17:42:07 -0300

    Previsao de hoje layout completo

--- src/pages/Main/PrevisaoDeHoje.js
+++ src/pages/Main/PrevisaoDeHoje.js
@@ -28,5 +28,35 @@ export default () => (
                 <p>14;03 Fev 19</p>
             </div>
         </div>
+        <div className="previsao-de-hoje__stat">
+            <div className="previsao-de-hoje__stat--row">
+                <p>Wind</p>
+                <p>Gentle Breeze, 4.6 m/s, East (90)</p>
+            </div>
+            <div className="previsao-de-hoje__stat--row">
+                <p>Cloudiness</p>
+                <p>Scattered clouds</p>
+            </div>
+            <div className="previsao-de-hoje__stat--row">
+                <p>Pressure</p>
+                <p>1009 hpa</p>
+            </div>
+            <div className="previsao-de-hoje__stat--row">
+                <p>Humidity</p>
+                <p>74%</p>
+            </div>
+            <div className="previsao-de-hoje__stat--row">
+                <p>Sunrise</p>
+                <p>06:02</p>
+            </div>
+            <div className="previsao-de-hoje__stat--row">
+                <p>Sunset</p>
+                <p>18:57</p>
+            </div>
+            <div className="previsao-de-hoje__stat--row">
+                <p>Geo coords</p>
+                <p>[-26.92, -49.07]</p>
+            </div>
+        </div>
     </div>
 );
